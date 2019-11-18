8993234b7f0560cd0871d19eb5c115f984a13050
deleone2 <deleone2@WITPF0TXF60.wit.private>
2019-11-18 17:46:26 -0500

    Sorting array Problem
    	Note: I did not implement the heap sort myself
    			(it wouldn't look so good)
    
    	-Find the minimum number of toys based on price a price
    		in an array of prices.
    	-Trick was to sort the array first then the problem becomes
    		simple enough.

---
+++ Personal_Projects/src/Challenge_Code/Mark_Toys.java
@@ -0,0 +1,111 @@
+package Challenge_Code;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Mark_Toys {
+
+    static int[] heapify(int arr[], int n, int i) {
+
+        int largest = i; // Initialize largest as root 
+        int l = 2*i + 1; // left = 2*i + 1 
+        int r = 2*i + 2; // right = 2*i + 2 
+  
+        // If left child is larger than root 
+        if (l < n && arr[l] > arr[largest]) 
+            largest = l; 
+  
+        // If right child is larger than largest so far 
+        if (r < n && arr[r] > arr[largest]) 
+            largest = r; 
+  
+        // If largest is not root 
+        if (largest != i) 
+        { 
+            int swap = arr[i]; 
+            arr[i] = arr[largest]; 
+            arr[largest] = swap; 
+  
+            // Recursively heapify the affected sub-tree 
+            heapify(arr, n, largest); 
+        } 
+
+        return(arr);
+    }
+    static int[] sort(int[] arr) {
+
+       int n = arr.length; 
+  
+        // Build heap (rearrange array) 
+        for (int i = n / 2 - 1; i >= 0; i--) 
+            heapify(arr, n, i); 
+  
+        // One by one extract an element from heap 
+        for (int i=n-1; i>=0; i--) 
+        { 
+            // Move current root to end 
+            int temp = arr[0]; 
+            arr[0] = arr[i]; 
+            arr[i] = temp; 
+  
+            // call max heapify on the reduced heap 
+            heapify(arr, i, 0); 
+        } 
+
+        return(arr);
+    }
+
+    // Complete the maximumToys function below.
+    static int maximumToys(int[] prices, int k) {
+
+        int cost = 0, maxToys = 0;
+
+            sort(prices);
+                
+            for (int i = 0; i < prices.length; i++) {
+                if (k >= prices[i] && k >= cost + prices[i]) {
+
+                    cost = cost + prices[i];
+                    maxToys++;
+                }
+            }
+
+        return(maxToys);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] prices = new int[n];
+
+        String[] pricesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pricesItem = Integer.parseInt(pricesItems[i]);
+            prices[i] = pricesItem;
+        }
+
+        int result = maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
